97d4c4f2c8ad361b0f1f70ffe05eaf004ec6327e
Divakar-2508 <divakaraugust@gmail.com>
2025-02-19 19:47:19 +0530

    grind engine

---
+++ src/app.rs
@@ -0,0 +1,24 @@
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"])]
+    async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+}
+
+#[derive(Serialize, Deserialize)]
+struct GreetArgs<'a> {
+    name: &'a str,
+}
+
+pub fn App() -> Element {
+    rsx! {
+        h1 {
+            "hello world"
+        }
+    }
+}
