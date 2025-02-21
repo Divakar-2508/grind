63daf7386d69011c885dab728f7dfe9c2e7b65cf
Divakar-2508 <divakaraugust@gmail.com>
2025-02-21 19:11:23 +0530

    app scaffold

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,7 @@
 #![allow(non_snake_case)]
 
 use dioxus::prelude::*;
+use grind_engine::models::Task;
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
@@ -15,10 +16,35 @@ struct GreetArgs<'a> {
     name: &'a str,
 }
 
-pub fn App() -> Element {
+static CSS: Asset = asset!("/assets/styles.css");
+
+pub async fn App() -> Element {
+    let tasks: Vec<Task> = invoke("get_tasks", JsValue::null()).await;
+
     rsx! {
+        document::Stylesheet { href: CSS }
         h1 {
-            "hello world"
+            "hello worldo"
+        }
+        h2 {
+            "baka baka"
+        }
+        for task in tasks {
+            TaskView { id: task.id, name: task.name }
+        }
+    }
+}
+
+#[component]
+fn TaskView(id: u32, name: String) -> Element {
+    rsx! {
+        div {
+            h1 {
+                {id}
+            }
+            h2 {
+                {name}
+            }
         }
     }
 }
